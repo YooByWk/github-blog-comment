84a8e02efb45073b1aff01a568c314ba81dab961
YooByWk <tls19190@naver.com>
2025-09-18 17:37:31 +0900

    feat: Cors

--- comment-api/src/main.rs
+++ comment-api/src/main.rs
@@ -11,6 +11,7 @@ use handlers::{AppState, add_comment, delete_comment, get_comments};
 use r2d2_sqlite::SqliteConnectionManager;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -20,12 +21,17 @@ async fn main() {
     init_db(&pool).expect("DB 초기화 실패");
 
     let state = AppState { pool };
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
 
     let app = Router::new()
         .route("/add", post(add_comment))
         .route("/list/{post}", get(get_comments))
         .route("/delete/{id}", post(delete_comment))
-        .with_state(state);
+        .with_state(state)
+        .layer(cors);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 7777));
     println!("서버 실행중: http://{}", addr);
